9cfdfba03c44a13b4700962be733aac45e9438f8
Kószó Tamás <koszotam@gmail.com>
2020-04-16 23:26:07 +0200

    string interpolation

--- src/app/character/character.component.ts
+++ src/app/character/character.component.ts
@@ -2,18 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-character',
-  template: `<p>I am Batman!</p>`,
-  styles: [`
-    p {
-      color: grey;
-    }
-  `]
+  template: `
+    <p>{{ 'Name:' }} {{ name }} Age: {{ 10 }} {{ showAffiliation() }}</p>
+  `,
+  styles: [``]
 })
 export class CharacterComponent implements OnInit {
+  public name = 'Batman';
+  public affiliation = 'Justice League';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public showAffiliation(): string {
+    return this.affiliation;
   }
-
 }
