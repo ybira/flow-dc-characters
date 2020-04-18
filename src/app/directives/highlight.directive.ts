5c31d29869f3aa6edbed35892f6549de0136be66
Kószó Tamás <koszotam@gmail.com>
2020-04-18 14:33:23 +0200

    renderer2, host listener

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,12 +1,31 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostListener,
+  OnInit,
+  Renderer2
+} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective implements OnInit {
-  constructor(private readonly elementRef: ElementRef) {}
-  public ngOnInit(): void {
-    this.elementRef.nativeElement.style.boxShadow =
-      '5px 5px 4px rgba(0, 0, 0, .5)';
+  constructor(
+    private readonly elementRef: ElementRef,
+    private readonly renderer: Renderer2
+  ) {}
+
+  public ngOnInit(): void {}
+
+  @HostListener('mouseenter') public mouseEnter() {
+    this.renderer.setStyle(
+      this.elementRef.nativeElement,
+      'box-shadow',
+      '5px 5px 4px rgba(0, 0, 0, .5)'
+    );
+  }
+
+  @HostListener('mouseleave') public mouseLeave() {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'box-shadow', 'none');
   }
 }
