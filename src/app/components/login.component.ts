4c4a82ebf337019352c61de7b17ac15ff8ef7ca3
Kószó Tamás <koszotam@gmail.com>
2020-04-21 14:17:42 +0200

    login TD form

--- src/app/components/login.component.ts
+++ src/app/components/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -6,27 +7,29 @@ import { AuthService } from '../services/auth.service';
   selector: 'app-login',
   template: `
     <div class="panel panel-default">
-      <div class="form-group">
-        <label for="email">Email</label>
-        <input
-          type="text"
-          id="email"
-          class="form-control"
-          [(ngModel)]="email"
-        />
-      </div>
-      <div class="form-group">
-        <label for="password">Password</label>
-        <input
-          type="text"
-          class="form-control"
-          id="password"
-          [(ngModel)]="password"
-        />
-      </div>
-      <button class="btn btn-primary" type="submit" (click)="onLogin()">
-        Login
-      </button>
+      <form (ngSubmit)="onLogin(form)" #form="ngForm">
+        <div class="form-group" [ngClass]="{ 'has-error': email.invalid && email.touched }">
+          <label class="control-label" for="email">Email</label>
+          <input type="text" id="email" class="form-control" name="email" #email="ngModel" ngModel required email />
+          <span class="help-block" *ngIf="email.invalid && email.touched">Email is invalid!</span>
+        </div>
+        <div class="form-group" [ngClass]="{ 'has-error': password.invalid && password.touched }">
+          <label class="control-label" for="password">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            name="password"
+            #password="ngModel"
+            ngModel
+            required
+          />
+          <span class="help-block" *ngIf="password.invalid && password.touched">Password is required!</span>
+        </div>
+        <button class="btn btn-primary" type="submit" [disabled]="form.invalid">
+          Login
+        </button>
+      </form>
     </div>
   `,
   styles: [
@@ -38,20 +41,17 @@ import { AuthService } from '../services/auth.service';
         width: 40%;
         margin-top: 10%;
       }
-    `
-  ]
+    `,
+  ],
 })
 export class LoginComponent implements OnInit {
-  public email: string;
-  public password: string;
-
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {}
 
-  public onLogin() {
+  public onLogin(form: NgForm) {
     this.authService
-      .login(this.email)
+      .login(form.value.email)
       .then(() => {
         this.router.navigate(['characters']);
       })
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,18 +5,16 @@ import { Role, User } from '../model/user.model';
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   private users: User[] = [
-    { id: 1, fullName: 'Admin Test', email: 'admin', role: Role.ADMIN },
-    { id: 2, fullName: 'Editor Test', email: 'editor', role: Role.EDITOR },
-    { id: 3, fullName: 'Reader Test', email: 'reader', role: Role.READER }
+    { id: 1, fullName: 'Admin Test', email: 'admin@test.com', role: Role.ADMIN },
+    { id: 2, fullName: 'Editor Test', email: 'editor@test.com', role: Role.EDITOR },
+    { id: 3, fullName: 'Reader Test', email: 'reader@test.com', role: Role.READER },
   ];
-  private loggedInUser$: BehaviorSubject<User> = new BehaviorSubject<User>(
-    null
-  );
+  private loggedInUser$: BehaviorSubject<User> = new BehaviorSubject<User>(null);
 
   public login(email: string): Promise<boolean> {
     return new Promise<boolean>((resolve, reject) => {
       setTimeout(() => {
-        const user = this.users.find(u => u.email === email);
+        const user = this.users.find((u) => u.email === email);
         if (user) {
           this.loggedInUser$.next(user);
           resolve(true);
